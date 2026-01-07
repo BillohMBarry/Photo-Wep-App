4ba011bbab3f481c48ea252e6968933ea0a26d5e
BillohMBarry <alhajibillohbarrie@gmail.com>
2026-01-07 05:15:00 -0800

    connect single image handler to D1 database and fix function name typo

--- src/handlers/get_single_image.ts
+++ src/handlers/get_single_image.ts
@@ -1,14 +1,34 @@
-import { ALL_IMAGES } from "../data/image_store";
+// import { ALL_IMAGES } from "../data/image_store";
 import { IRequest } from "itty-router";
+import { Env } from "../env";
 
-const getSongleImage = (request: IRequest) => {
-    let imgageID = ALL_IMAGES.find(image => image.id.toString() == request.params.id);
-
-    if (!imgageID) {
+const getSingleImage = async (request: IRequest, env: Env) => {
+    let result
+    console.log("getSingleImage params:", request.params); // Debug log
+    try {
+        result = await env.photo_web_app_DB.prepare(`
+            SELECT i.*, c.display_name AS category_display_name
+            FROM images i
+            INNER JOIN image_categories c ON i.category_id = c.id
+            WHERE i.id = ?1`
+        )
+            .bind(Number(request.params.id))
+            .first();
+        console.log("getSingleImage result:", result); // Debug log
+    } catch (e) {
+        let message
+        if (e instanceof Error) message = e.message
+        console.error({
+            message: message,
+            error: e
+        })
+        return new Response("Internal Server Error", { status: 500 });
+    }
+    if (!result) {
         return new Response("Image not found", { status: 404 });
     }
-    return new Response(JSON.stringify(imgageID), {
+    return new Response(JSON.stringify(result), {
         headers: { "Content-Type": "application/json" }
     });
 }
-export default getSongleImage;
+export default getSingleImage;
